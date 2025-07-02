5ae57b35cea35cfef0d734d0e11e4f878138a5ab
hjfuentes <hugojoel.fuentes@gmail.com>
2025-07-01 22:39:55 -0500

    Primer commit 10:39pm

---
+++ Python_para_principiantes/01_Hola_python.py
@@ -0,0 +1,7 @@
+#Autor: Hugo Joel Fuentes
+#Fecha: 01-Jul-2025
+#Comentario: primer programa en python
+print("\n==================================================")
+print("\tHola python, desde Visual Studio Code y con Git...")
+print("\tScript ha finalizado.")
+print("==================================================\n")
