4bfebf093931af02b5d70dc3e096e5a81efeb43d
hjfuentes <hugojoel.fuentes@gmail.com>
2025-07-01 22:43:22 -0500

    actualizacion a las 10:40pm

--- Python_para_principiantes/01_Hola_python.py
+++ Python_para_principiantes/01_Hola_python.py
@@ -2,6 +2,6 @@
 #Fecha: 01-Jul-2025
 #Comentario: primer programa en python
 print("\n==================================================")
-print("\tHola python, desde Visual Studio Code y con Git...")
-print("\tScript ha finalizado.")
+print("\tHola python desde Visual Studio Code y con Git...")
+print("\tScript ha finalizado....")
 print("==================================================\n")
